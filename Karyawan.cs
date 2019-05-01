6a2ca9c391af688c09a3da168b25cfbfdbde4938
Nur Fikri <Muhammadnurfikri286@gmail.com>
2019-05-02 00:13:50 +0700

    Responsi

---
+++ Karyawan.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrog2338
+{
+    class Karyawan
+    {
+        public string Nik { get; set; }
+        public string Nama { get; set; }
+        public int Gaji { get; set; }
+        public int Bonus { get; set; }
+
+        public Karyawan ( string nik, string nama, int gaji)
+        {
+            Nik = nik;
+            Nama = nama;
+            Gaji = gaji;
+            Bonus = gaji + (gaji / 100 * 10);
+
+            if (gaji < 0)
+            {
+                gaji = 0;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrog2338
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Responsi Pemrograman ";
+
+            Karyawan ky1 = new Karyawan("12342332", "Nur  ", 100000);
+            Karyawan ky2 = new Karyawan("12342333","Fikri",200000);
+
+            Console.WriteLine(" Nik          Nama        Gaji bulanan");
+            Console.WriteLine("--------------------------------------");
+            Cetakhasil(ky1);
+            Cetakhasil(ky2);
+
+            Console.WriteLine("\n---------------------------------");
+            Console.WriteLine("| Akhirnya Kenaikan Gaji 10%!!! |");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("\n Nik          Nama        Gaji bulanan");
+            Console.WriteLine("--------------------------------------");
+            Cetakbonus(ky1);
+            Cetakbonus(ky2);
+            Console.ReadKey();
+
+        }
+        static void Cetakhasil(Karyawan ky)
+        {
+            Console.WriteLine(" {0}     {1}     {2}", ky.Nik, ky.Nama, ky.Gaji);   
+        }
+
+        static void Cetakbonus(Karyawan ky)
+        {
+            Console.WriteLine(" {0}     {1}     {2}", ky.Nik, ky.Nama, ky.Bonus);
+        }
+    }
+}
